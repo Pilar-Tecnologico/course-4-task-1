2e4592f930f62914b4f587c7fb112ef59deb82c3
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2021-05-26 18:05:44 -0300

    minor comments fix

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 const playersData = require('');//should require the data.json file
+app.use(express.json());
 
 app.get('/players', (req, res) => {
     //should respond with the "players" array inside playersData and Status 200    
@@ -11,7 +12,7 @@ app.get('/players', (req, res) => {
 
 app.get('/players/:role', (req, res) => {
     //should respond with only the players that have with the especified role. Status 200.
-    //If there's no player with the specified role it should respond with {"error": "No player found"} and Status 409.
+    //If there's no player with the specified role it should respond with {"error": "No player found"} and Status 404.
 });
 
 app.put('/players', (req, res) => {
@@ -20,7 +21,7 @@ app.put('/players', (req, res) => {
     //Response should be {"operation": "add player", "status": "accepted"} with status 200 if the body request is valid.
     //Response should be {"operation": "add player", "status": "refused", "details": "Invalid body"} with status 409 if any property is missing.
     //The Only valid properties are the ones at every player object in data.json.
-})
+});
 
 app.listen(port, () => {
     console.log('Express server started at port ' + port)
