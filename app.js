e93d851e724a4e07cc4e35317dd5c7f0d48bddc7
Marcos J. Peña Pollastri <marcos.p.pollastri@gmail.com>
2021-05-26 17:13:30 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,27 @@
+//Change the version of this program in package.json to 1.1.0
+//For all the excersices Postman is recommended.
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const playersData = require('');//should require the data.json file
+
+app.get('/players', (req, res) => {
+    //should respond with the "players" array inside playersData and Status 200    
+});
+
+app.get('/players/:role', (req, res) => {
+    //should respond with only the players that have with the especified role. Status 200.
+    //If there's no player with the specified role it should respond with {"error": "No player found"} and Status 409.
+});
+
+app.put('/players', (req, res) => {
+    //Should recive player data from request body.
+    //Should console.log the response.
+    //Response should be {"operation": "add player", "status": "accepted"} with status 200 if the body request is valid.
+    //Response should be {"operation": "add player", "status": "refused", "details": "Invalid body"} with status 409 if any property is missing.
+    //The Only valid properties are the ones at every player object in data.json.
+})
+
+app.listen(port, () => {
+    console.log('Express server started at port ' + port)
+});
