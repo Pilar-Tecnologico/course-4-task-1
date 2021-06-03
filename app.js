d53ff2668fba1aae1ab022937656476a85eda4a4
Maximiliano Mercado Diaz <50929240+maximarsoficial@users.noreply.github.com>
2021-06-02 22:00:59 -0300

    last activity carried out

--- app.js
+++ app.js
@@ -21,25 +21,36 @@ app.get('/players', (req, res) => {
 });
 
 app.get('/players/:role', (req, res) => {
-    //should respond with only the players that have with the especified role. Status 200.
-    
+   
+  //should respond with only the players that have with the especified role. Status 200.
     const roles= req.params.role;
     const plRole= playersData.players.filter(elem => elem.role === roles)
     if(plRole.length>0){
         res.status(200).json(plRole)
     }
+    //If there's no player with the specified role it should respond with {"error": "No player found"} and Status 404.
     else{
         res.status(404).json({"error": 'Player not found'})
     }
-    //If there's no player with the specified role it should respond with {"error": "No player found"} and Status 404.
 });
 
 app.put('/players', (req, res) => {
     //Should recive player data from request body.
-    //Should console.log the response.
+     //Should console.log the response.
     //Response should be {"operation": "add player", "status": "accepted"} with status 200 if the body request is valid.
     //Response should be {"operation": "add player", "status": "refused", "details": "Invalid body"} with status 409 if any property is missing.
     //The Only valid properties are the ones at every player object in data.json.
+    
+    const reqBody = req.params.body;
+    let playersDataAdd = (playersData.name && playersData.lastname && playersData.role && playersData.team)    
+    if (playersDataAdd === reqBody){
+      res.status(200).json({"operation": "add player", "status": "accepted"});
+      console.log('"operation": "add player", "status": "accepted"');
+    }else{
+      res.status(409).json({"operation": "add player", "status": "refused", "details": "Invalid body"});
+      console.log('"operation": "add player", "status": "refused", "details": "Invalid body"');
+    }  
+   
 });
 
 app.listen(port, () => {
